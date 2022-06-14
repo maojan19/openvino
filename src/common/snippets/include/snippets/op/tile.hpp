86eb05fe26b3e7985186a064ef4d5a308f279d34
MaoJan <maojan19@gmail.com>
2022-06-14 13:12:23 +0300

    Pass increment and dims to op::Tile constructor

--- src/common/snippets/include/snippets/op/tile.hpp
+++ src/common/snippets/include/snippets/op/tile.hpp
@@ -20,7 +20,7 @@ class Tile : public ngraph::op::Op {
 public:
     OPENVINO_OP("Tile", "SnippetsOpset");
 
-    Tile(const std::vector<AllocatedEmitter>& region);
+    Tile(const std::vector<AllocatedEmitter>& region, size_t increment, size_t num_inputs, size_t num_outputs, const std::vector<size_t>& io_dims);
     Tile() = default;
     std::vector<AllocatedEmitter> region;
     size_t increment = 0;
@@ -29,7 +29,7 @@ public:
     std::vector<size_t> io_dims {};
 
     std::shared_ptr<Node> clone_with_new_inputs(const OutputVector& inputs) const override {
-        return std::make_shared<Tile>(region);
+        return std::make_shared<Tile>(region, increment, num_inputs, num_outputs, io_dims);
     }
 };
 
