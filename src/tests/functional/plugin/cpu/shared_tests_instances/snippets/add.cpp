211520ffb0b23f2b3264f86fe023e35852ede1ff
MaoJan <maojan19@gmail.com>
2022-06-29 12:00:35 +0300

    Extended dynamic AddSinh tests

--- src/tests/functional/plugin/cpu/shared_tests_instances/snippets/add.cpp
+++ src/tests/functional/plugin/cpu/shared_tests_instances/snippets/add.cpp
@@ -38,26 +38,91 @@ INSTANTIATE_TEST_SUITE_P(smoke_Snippets_Eltwise, AddSinhConst,
                              ::testing::Values(CommonTestUtils::DEVICE_CPU)),
                      AddSinhConst::getTestCaseName);
 
-//InputShape inShapesDynamic1 = {{16, 29, ngraph::Dimension(1, 512)}, {{16, 29, 17}}};
-//std::vector<InputShape> inShapesDynamic2 = {
-//        {{16, 29, ngraph::Dimension(1, 512)}, {{16, 29, 17}}}, // no broadcasting on inner Tile => Ok
-//        {{16, 29, ngraph::Dimension(1, 512)}, {{16, 29, 1}}}
-//    };
+namespace snippets_dynamic_1 {
+    InputShape inShapesDynamic1 = {{16, 6, ngraph::Dimension(1, 512)}, {{16, 6, 16}}};
+    std::vector<InputShape> inShapesDynamic2 = {{{16, 6, ngraph::Dimension(1, 512)}, {{16, 6, 16}}},
+                                                {{16, 6, ngraph::Dimension(1, 512)}, {{16, 6, 1}}}};
 
-InputShape inShapesDynamic1 = {{16, 6, ngraph::Dimension(1, 512)}, {{16, 6, 16}}};
-std::vector<InputShape> inShapesDynamic2 = {
-            {{16, 6, ngraph::Dimension(1, 512)}, {{16, 6, 16}}}, // no broadcasting on inner Tile => Ok
-            {{16, 6, ngraph::Dimension(1, 512)}, {{16, 6, 1}}}
-};
+    INSTANTIATE_TEST_SUITE_P(
+        smoke_Snippets_Eltwise,
+        AddSinhDynamic,
+        ::testing::Combine(::testing::Values(inShapesDynamic1),
+                           ::testing::ValuesIn(inShapesDynamic2),
+                           ::testing::Values(3),  // Add + 2 converts after inputs
+                           ::testing::Values(1),  // Subgraph is created, since the inputs are followed by converts
+                           ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+        AddSinhDynamic::getTestCaseName);
+} // namespace snippets_dynamic_1
 
-INSTANTIATE_TEST_SUITE_P(smoke_Snippets_Eltwise, AddSinhDynamic,
-                         ::testing::Combine(
-                             ::testing::Values(inShapesDynamic1),
-                             ::testing::ValuesIn(inShapesDynamic2),
-                             ::testing::Values(3), // Add + 2 converts after inputs
-                             ::testing::Values(1), // Subgraph is created, since the inputs are followed by converts
-                             ::testing::Values(CommonTestUtils::DEVICE_CPU)),
-                         AddSinhDynamic::getTestCaseName);
+namespace snippets_dynamic_2 {
+    InputShape inShapesDynamic1 = {{16, 6, ngraph::Dimension(1, 512)}, {{16, 6, 7}}};
+    std::vector<InputShape> inShapesDynamic2 = {{{16, 6, ngraph::Dimension(1, 512)}, {{16, 6, 7}}},
+                                                {{16, 6, ngraph::Dimension(1, 512)}, {{16, 6, 1}}}};
+
+    INSTANTIATE_TEST_SUITE_P(
+        smoke_Snippets_Eltwise,
+        AddSinhDynamic,
+        ::testing::Combine(::testing::Values(inShapesDynamic1),
+                           ::testing::ValuesIn(inShapesDynamic2),
+                           ::testing::Values(3),  // Add + 2 converts after inputs
+                           ::testing::Values(1),  // Subgraph is created, since the inputs are followed by converts
+                           ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+        AddSinhDynamic::getTestCaseName);
+} // namespace snippets_dynamic_2
+
+namespace snippets_dynamic_3 {
+InputShape inShapesDynamic1 = {{16, 1, ngraph::Dimension(1, 512)}, {{16, 1, 16}}};
+std::vector<InputShape> inShapesDynamic2 = {{{16, 1, ngraph::Dimension(1, 512)}, {{16, 1, 16}}},
+                                            {{16, 1, ngraph::Dimension(1, 512)}, {{16, 1, 1}}}};
+
+INSTANTIATE_TEST_SUITE_P(
+    smoke_Snippets_Eltwise,
+    AddSinhDynamic,
+    ::testing::Combine(::testing::Values(inShapesDynamic1),
+                       ::testing::ValuesIn(inShapesDynamic2),
+                       ::testing::Values(3),  // Add + 2 converts after inputs
+                       ::testing::Values(1),  // Subgraph is created, since the inputs are followed by converts
+                       ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+    AddSinhDynamic::getTestCaseName);
+} // namespace snippets_dynamic_3
+
+namespace snippets_dynamic_4 {
+InputShape inShapesDynamic1 = {{16, 1, ngraph::Dimension(1, 512)}, {{16, 1, 7}}};
+std::vector<InputShape> inShapesDynamic2 = {{{16, 1, ngraph::Dimension(1, 512)}, {{16, 1, 7}}},
+                                            {{16, 1, ngraph::Dimension(1, 512)}, {{16, 1, 1}}}};
+
+INSTANTIATE_TEST_SUITE_P(
+    smoke_Snippets_Eltwise,
+    AddSinhDynamic,
+    ::testing::Combine(::testing::Values(inShapesDynamic1),
+                       ::testing::ValuesIn(inShapesDynamic2),
+                       ::testing::Values(3),  // Add + 2 converts after inputs
+                       ::testing::Values(1),  // Subgraph is created, since the inputs are followed by converts
+                       ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+    AddSinhDynamic::getTestCaseName);
+} // namespace snippets_dynamic_4
+
+// todo: tests with {16, 6, 16} + {16, 6, 1} work fine, but {16, 6, 1} + {16, 6, 16} doesnt' pass because
+//  src memory ptr on the 1st input == dst memory ptr, so input gets rewritten in the last test. This is
+//  not a Snippets problem, but a memory reuse feature. Uncomment this test when the issue is resolved.
+//namespace snippets_dynamic_5 {
+//InputShape inShapesDynamic1 = {{16, 1, ngraph::Dimension(1, 512)}, {{16, 1, 1}}};
+//std::vector<InputShape> inShapesDynamic2 = {{{16, 1, ngraph::Dimension(1, 512)}, {{16, 1, 1}}},
+//                                            {{16, 1, ngraph::Dimension(1, 512)}, {{16, 1, 7}}},
+//                                            {{16, 1, ngraph::Dimension(1, 512)}, {{16, 1, 16}}},
+//                                            {{16, 1, ngraph::Dimension(1, 512)}, {{16, 1, 31}}}};
+//
+//INSTANTIATE_TEST_SUITE_P(
+//    smoke_Snippets_Eltwise,
+//    AddSinhDynamic,
+//    ::testing::Combine(::testing::Values(inShapesDynamic1),
+//                       ::testing::ValuesIn(inShapesDynamic2),
+//                       ::testing::Values(3),  // Add + 2 converts after inputs
+//                       ::testing::Values(1),  // Subgraph is created, since the inputs are followed by converts
+//                       ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+//    AddSinhDynamic::getTestCaseName);
+//} // namespace snippets_dynamic_5
+
 }  // namespace
 } // namespace snippets
 } // namespace test
